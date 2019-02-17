a7cb1669b07771568093125944c25ae2da37c774
haloreaper <haloreaper101@gmail.com>
2019-02-16 18:52:19 -0800

    Update josephG.cpp

--- josephG.cpp
+++ josephG.cpp
@@ -2,19 +2,7 @@
 //program: josephG.cpp
 //author: Joseph Gaede
 //
-#include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <unistd.h>
-#include <ctime>
-#include <cmath>
-#include <X11/Xlib.h>
-//#include <X11/Xutil.h>
-//#include <GL/gl.h>
-//#include <GL/glu.h>
-#include <X11/keysym.h>
 #include <GL/glx.h>
-#include "log.h"
 #include "fonts.h"
    
 
