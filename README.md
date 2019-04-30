17328ff64ad6df642da9cf2867b0790a89a7b6c2
Sharadeos <shadow_zeny@hotmail.com>
2019-04-30 15:54:10 -0700

    Rebuild

---
+++ README.md
@@ -0,0 +1,17 @@
+Main Google Document:
+
+https://docs.google.com/document/d/1Pgl766NKwfj5RFielhsRwOVK6ev_fVXwoZ62HE9U__U/edit
+
+Members:
+
+Andrew (Joey) Hubbard | (Chris) Ramirez | (Adolfo) Valencia | Jose (Luis) Manahan | (Joseph) Gaede
+
+Changelog:
+
+2/22.19 Created Individual Branches
+
+2/27/19 Updated ReadMe
+
+4/7/19 Restructued the code to have singleton functionality and freely access Game g and ability to use classes from different files.
+
+4/16/19 Removed reliance on global variables, now the game will rescale to the size of the display and all of the objects rendered as well.
---
+++ andrewH.cpp
@@ -0,0 +1,46 @@
+//
+//program: andrewH.cpp
+//author: Andrew (Joey) Hubbard
+//Last Modified: 2-18-2019 10:45am
+//
+#include <GL/glx.h>
+#include "fonts.h"
+#include <math.h>
+static float spin = 20;
+void andrewH(int x, int y, GLuint textid, float move) 
+{
+    if(!move)
+	spin = 20;
+
+    Rect r;
+    unsigned int c = 0x00ffff44;
+    r.bot = y;
+    r.left = x;
+    r.center = 0;
+    ggprint8b(&r, 16, c, "Andrew Hubbard");
+
+    Rect q; 
+    q.bot = y;
+    q.left = x/2;
+    q.center = 0;
+    ggprint16(&q, 16, c, "CREATED BY");
+    
+    float wid = 110.0f; 
+    float rad = move * -.05;
+    glPushMatrix();
+    glTranslatef(x + 250, y, 0);
+    glRotatef(spin*sin(rad),0,0,1);
+    glBindTexture(GL_TEXTURE_2D, textid);
+    glEnable(GL_ALPHA_TEST);
+    glAlphaFunc(GL_GREATER, 0.0f);
+    glColor4ub(255,255,255,255);
+    glBegin(GL_QUADS);
+        glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
+        glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+        glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
+        glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
+    glEnd();
+    glPopMatrix();
+    if (sin(rad) > .98 || sin(rad) < -.98)
+    	spin *= 1.01;
+}
